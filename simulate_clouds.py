590550e3077d67ac4b69efd9eb24083d729fad99
WiktorSwiatkowski <wiktorswiatkowski1@gmail.com>
2023-03-20 18:42:05 +0100

    Program do dopasowania powierzchni

--- simulate_clouds.py
+++ simulate_clouds.py
@@ -4,8 +4,8 @@ import numpy as np
 
 
 def generate_points(num_points: int = 2000):
-    distribution_x = norm(loc=-50, scale=15)
-    distribution_y = norm(loc=0, scale=100)
+    distribution_x = norm(loc=75, scale=10)
+    distribution_y = norm(loc=0, scale=20)
     distribution_z = norm(loc=0.2, scale=0.05)
 
     x = distribution_x.rvs(size=num_points)
@@ -17,9 +17,9 @@ def generate_points(num_points: int = 2000):
 
 
 def generate_points_pion(num_points: int = 2000):
-    distribution_x = norm(loc=-100, scale=0.05)
-    distribution_y = norm(loc=0, scale=100)
-    distribution_z = norm(loc=0.2, scale=20)
+    distribution_x = norm(loc=150, scale=0.05)
+    distribution_y = norm(loc=0, scale=20)
+    distribution_z = norm(loc=0.2, scale=10)
 
     x = distribution_x.rvs(size=num_points)
     y = distribution_y.rvs(size=num_points)
@@ -29,7 +29,7 @@ def generate_points_pion(num_points: int = 2000):
     return points
 
 
-def generate_points_cyl(radius=10, height=50, density=1000, turns=40):
+def generate_points_cyl(radius=10, height=50, density=5000, turns=400):
     angle = np.linspace(0, turns * np.pi, density)
     x = radius * np.cos(angle)
     y = radius * np.sin(angle)
